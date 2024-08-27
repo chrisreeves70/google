c0e5bd3bd418f0a8ea120967dc7cbb3093fdc086
chrisreeves70 <chrisreeves70@icloud.com>
2024-08-27 12:55:35 -0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <div class="container">
-        <h1 class="mt-5">Welcome to the Activity 4 Cloud App (AWS)</h1>
+        <h1 class="mt-5">Welcome to the Activity 4 Cloud App (Google Cloud)</h1>
         <a href="add_user.php" class="btn btn-primary mt-3">Add User</a>
         <a href="view_users.php" class="btn btn-secondary mt-3">View Users</a>
     </div>
