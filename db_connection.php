3f28d5ea1da3e53acb341adb74432456541a6749
chrisreeves70 <chrisreeves70@icloud.com>
2024-08-27 22:10:19 -0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,14 +1,16 @@
 <?php
-$servername = "35.225.245.211"; // Your Google Cloud MySQL public IP address
+// Database connection settings
+$servername = "104.154.99.108"; // Your Google Cloud MySQL public IP address
 $username = "root"; // Your MySQL username
 $password = "Scout1st"; // Your MySQL password
-$dbname = "alpha_db"; // Replace with your actual database name
+$dbname = "applicationdb"; // Replace with your actual database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
+    // Log the error message and display a generic error message to the user
     error_log("Connection failed: " . $conn->connect_error);
     die("Connection failed: Please check your database credentials.");
 }
