d8fb51d453d1fc6602673bc1daa8cc4224aa8903
chrisreeves70 <chrisreeves70@icloud.com>
2024-08-27 21:16:48 -0700

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include 'db_connection.php';
 </head>
 <body>
     <div class="container">
-        <h1 class="mt-5">Welcome to the Home Page</h1>
+        <h1 class="mt-5">Welcome to Activity 4 (Google)</h1>
         <a href="add_user.php" class="btn btn-primary">Add User</a>
         <a href="view_users.php" class="btn btn-secondary">View Users</a>
     </div>
